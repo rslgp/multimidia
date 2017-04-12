d72defe327058857c60af8d790b02de209fedbd6
rslgp <rslgp@cin.ufpe.br>
2017-04-12 09:52:04 -0300

    inseriu girarVideo e padraoParametros para enxugar os metodos para usar somente o core do metodo

--- src/IntegracaoBasicaFFmpegOld.java
+++ src/IntegracaoBasicaFFmpegOld.java
@@ -1,6 +1,6 @@
 import java.util.LinkedList;
 
-public class IntegracaoBasicaFFmpeg {
+public class IntegracaoBasicaFFmpegOld {
 	
 	final static String[] verbose = {"-loglevel","panic"};
 	final static String pastaRaiz= "C:\\Users\\rslgp\\Downloads\\multimidia-master\\src\\";
@@ -15,7 +15,6 @@ public class IntegracaoBasicaFFmpeg {
 		
 		processoFFmpeg.waitFor();
 	}
-	
 	/*
 	 * para mostrar o progresso do ffmpeg, coloca antes do parametro ffmpeg 
 //				"cmd",
@@ -68,31 +67,100 @@ public class IntegracaoBasicaFFmpeg {
 	}
 
 	public static String[] cortar(String inicio, String fim) throws Exception{
+//		String input = enderecoArquivoVideo+"video.mp4",
+//				output = enderecoArquivoVideo+"split.mp4";
+//				
+//		String[] parameters={
+//				ffmpeg,
+//				verbose[0],
+//				verbose[1],
+//				"-y",
+//				"-i",
+//				input,
+//				"-ss",
+//				inicio,
+//				"-c",
+//				"copy",
+//				"-t",
+//				fim,//"00:00:30.0"
+//				output
+//		};
+		
 		String input = "video.mp4",
 				output = "split.mp4";
-		
 		String[] core={
-			"-ss",
-			inicio,
-			"-c",
-			"copy",
-			"-t",
-			fim,//"00:00:30.0"				
+				"-ss",
+				inicio,
+				"-c",
+				"copy",
+				"-t",
+				fim,//"00:00:30.0"				
 		};
 		
 		return padraoParametros(input, output, core);
 	}
 	
 	public static String[] makeVideoResizedBorder() throws Exception{
+//		String input = enderecoArquivoVideo+"split.mp4",
+//				output = enderecoArquivoVideo+"out.mp4";
+//		String[] parameters={
+//				ffmpeg, 
+//				"-y",
+//				"-i",
+//				input,
+//				"-f",
+//				"lavfi",
+//				"-i",
+//				"color=c=black:s=1920x1080",
+//				"-filter_complex",
+//				"\"[0:v]scale=w=0.80*iw:h=0.80*ih[scaled];",
+//				"[1:v][scaled]overlay=x=0.10*main_w:y=0.10*main_h:eof_action=endall[out];",
+//				"[0:a]anull[aud]\"",
+//				"-map",
+//				"\"[out]\"",
+//				"-map",
+//				"\"[aud]\"",
+//				"-strict",
+//				"-2",
+//				output
+//		};
+		
+//		String[] parameters={
+//				ffmpeg,
+//				"-y",
+//				"-i",
+//				input,
+//				"-filter_complex",
+//				"\"scale=578:462,pad=720:576:71:57\"",
+//				output
+//		};
+		
+//		String[] parameters={
+////				"cmd",
+////				"/c",
+////				"start",
+//				ffmpeg,
+//				verbose[0],
+//				verbose[1],
+//				"-y",
+//				"-i",
+//				input,
+//				"-filter_complex",
+//				"\"scale=578:462,pad=720:576:71:57\"",
+//				output
+//		};
+
+//		imprimirParametros(parameters);
+		
+
 		String input = "split.mp4",
 				output = "out.mp4";
-		
 		String[] core={
-			"-filter_complex",
-			"\"scale=578:462,pad=720:576:71:57\""				
+				"-filter_complex",
+				"\"scale=578:462,pad=720:576:71:57\""				
 		};		
+		return padraoParametros(input, output, core);
 		
-		return padraoParametros(input, output, core);		
 	}
 	
 	public static String getScreenMosaic(int larguraScreen, int alturaScreen, int larguraPip, int alturaPip, int espacamentoAlturaEntrePip){
@@ -119,7 +187,33 @@ public class IntegracaoBasicaFFmpeg {
 	}
 	
 	public static String[] mosaic() throws Exception{
-		String input = "split.mp4",
+//		String input1 = enderecoArquivoVideo+"split.mp4",
+//				input2 = enderecoArquivoVideo+"split.mp4",
+//						input3 = enderecoArquivoVideo+"split.mp4",
+//				output = enderecoArquivoVideo+"out.mp4";
+
+//		String[] parameters={
+//				ffmpeg,
+//				verbose[0],
+//				verbose[1],
+//				"-y",
+//				"-i",
+//				input1,
+//				"-i",
+//				input2,
+//				"-i",
+//				input3,
+//				"-filter_complex",
+//				getScreenMosaic(1920,1080,599,336,2),
+//				"-c:v",
+//				"libx264",
+//				output
+//		};
+
+//		imprimirParametros(parameters);
+		
+
+		String input1 = "split.mp4",
 				input2 = "split.mp4",
 				input3 ="split.mp4",
 				output = "out.mp4";
@@ -134,11 +228,41 @@ public class IntegracaoBasicaFFmpeg {
 				"-c:v",
 				"libx264"				
 		};
+
+//		executarFFmpeg(parameters);
 		
-		return padraoParametros(input, output, core);
+		return padraoParametros(input1, output, core);
 	}
 	
-	public static String[] addText(String texto) throws Exception{		
+	
+//	public static String getText(){
+//		String fonte="\""+enderecoArquivoFonteTexto+"/HelveticaNeueLTStd-Th_1.otf\"",
+//				time="enable='between(t,0,10)'",
+//				texto="teste";
+//		
+//		return "-vf drawtext="+time+":fontfile="+fonte+":text=\'"+texto+"\':fontcolor=white:fontsize=40:x=0:y=0";
+//	}
+	
+	public static String[] addText(String texto) throws Exception{
+//		String input = enderecoArquivoVideo+"out.mp4",
+//				output = enderecoArquivoVideo+"text.mp4",
+//				fonte="\""+enderecoArquivoFonteTexto+"/HelveticaNeueLTStd-Th_1.otf\"",
+//				time="enable='between(t,0,3)'";
+				
+//		String[] parameters={
+//				ffmpeg,
+//				verbose[0],
+//				verbose[1],
+//				"-y",
+//				"-i",
+//				input,
+//				"-vf",
+//				"drawtext="+time+":fontfile="+fonte+":text=\'"+texto+"\':fontcolor=white:fontsize=40:x=0:y=0",
+//				"-codec:a",
+//				"copy",
+//				output
+//		};
+		
 		String input = "out.mp4",
 				output = "text.mp4",
 				fonte="\""+enderecoArquivoFonteTexto+"/HelveticaNeueLTStd-Th_1.otf\"",
@@ -151,27 +275,47 @@ public class IntegracaoBasicaFFmpeg {
 				"copy",
 		};
 		
+//		executarFFmpeg(parameters);
 //		System.out.println("\n"+processoFFmpeg.getErrorStream());
 		
 		return padraoParametros(input, output, core);
+		
+//		ffmpeg -i input.mp4 -vf drawtext="fontfile=/path/to/font.ttf: \
+//				text='Stack Overflow': fontcolor=white: fontsize=24: box=1: boxcolor=black@0.5: \
+//				boxborderw=5: x=(w-text_w)/2: y=(h-text_h)/2" -codec:a copy output.mp4
 	}	
 
 	public static void imprimirParametros(String[] parameters){
 		for(String i : parameters) System.out.print(i+" ");	
 		System.out.println("\r\n");
 	}
-	
 	public static String[] girarVideo(){
+//		String input = enderecoArquivoVideo+"split.mp4",
+//		output = enderecoArquivoVideo+"girado.mp4";
+//		
+//		String[] parameters={
+//		ffmpeg,
+//		verbose[0],
+//		verbose[1],
+//		"-y",
+//		"-i",
+//		input,
+//		"-vf",
+//		"\"transpose=1\"",
+//		output
+//		};
+		
+
 		String input = "split.mp4",
 		output = "girado.mp4";
 		
 		String[] core={
-			"-vf",
-			"\"transpose=1\""				
+				"-vf",
+				"\"transpose=1\""				
 		};
 		
 		return padraoParametros(input, output, core); 
-	}
+		}
 	static LinkedList<String[]> comandos;
 	
 	public static void main(String[] args) {
@@ -179,10 +323,10 @@ public class IntegracaoBasicaFFmpeg {
 		comandos = new LinkedList<String[]>();
 		try {
 			comandos.add(cortar("00:00:05.0","00:00:15.0"));
-			//comandos.add(girarVideo());
-			comandos.add(makeVideoResizedBorder());
-			comandos.add(mosaic());
-			comandos.add(addText("teste"));
+			comandos.add(girarVideo());
+//			comandos.add(makeVideoResizedBorder());
+//			comandos.add(mosaic());
+//			comandos.add(addText("OLAA ANDRE, ORION!"));
 			
 			for(String[] parametros : comandos) executarFFmpeg(parametros);
 			
@@ -191,4 +335,5 @@ public class IntegracaoBasicaFFmpeg {
 		} catch (Exception e){e.printStackTrace();}
 		
 	}
+
 }
