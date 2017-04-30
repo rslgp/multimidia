8b59cda72d0a18997bafaff65bafdc3f093d7342
rslgp <rslgp@cin.ufpe.br>
2017-04-30 16:37:20 -0300

    adicionou deletar, botao direito popup, integrou gui com ffmpeg, funcoes cortar, moisaco, texto, ajuste em moisaco, implementado moisaco dinamico, inseriu linha vermelha para servir de scale com a gui inserindo regra de tres

--- src/GUI.java
+++ src/GUI.java
@@ -2,14 +2,20 @@ import java.awt.AlphaComposite;
 import java.awt.BasicStroke;
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.GridLayout;
 import java.awt.KeyEventDispatcher;
 import java.awt.KeyboardFocusManager;
+import java.awt.Paint;
 import java.awt.Point;
 import java.awt.Polygon;
 import java.awt.Rectangle;
 import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -20,21 +26,29 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 import javax.swing.JComponent;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
+import javax.swing.JTextField;
 
 public class GUI extends JFrame {
-	
+	//contem classe gui e duas privadas Videobox e PaintSurface	
 	public String getDesktop(){return System.getProperty("user.home") + "\\Desktop";}
 	String caminhoSave = getDesktop()+"\\salvar.txt";
+	final String currentPath=GUI.class.getProtectionDomain().getCodeSource().getLocation().getPath().substring(1).replace('/', '\\');
 	
 	public GUI() {
-		this.setSize(300, 300);
+		this.setSize(620, 520);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		getContentPane().add(new PaintSurface(), BorderLayout.CENTER);	
-		this.setVisible(true);
+		this.setVisible(true);	
 	}
   
 	//classe que cria objetos que guardam a forma, endereco do video, id
@@ -57,21 +71,131 @@ public class GUI extends JFrame {
 			return padraoSave(retangulo);
 		}
 	}
+	//fim classe videobox
 
-	private class PaintSurface extends JComponent {
-		Graphics2D tela;
-		ArrayList<VideoBox> shapes = new ArrayList<VideoBox>();
+	private class PaintSurface extends JComponent {		
+		private final JPopupMenu popup;
+		
+		private JPanel panel,panel2;
+		private JTextField field1,field2,field3,field4,field5,field6;
+		
+		private Graphics2D tela;
+		private ArrayList<VideoBox> shapes = new ArrayList<VideoBox>();
 	
-		Point startDrag, endDrag;
+		private Point startDrag, endDrag;
 		
 		private Polygon dragged;
 		private Point lastLocation;
 		
 		boolean wantDraw=true;
-		int id=0;
+		private int id=1;
 	
-		JFileChooser selecionarVideo = new JFileChooser();
-		File workingDirectory = new File(System.getProperty("user.dir"));
+		private final JFileChooser selecionarVideo = new JFileChooser();
+		private final File workingDirectory = new File(System.getProperty("user.dir"));
+				
+		public PaintSurface() {
+			IntegracaoBasicaFFmpeg.comandos=new LinkedList<>();
+			
+			selecionarVideo.setCurrentDirectory(workingDirectory);
+						
+			//salvar ou load usando teclado
+			KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher( configTeclado	);
+			
+			//config desenhaArrasta
+			this.addMouseListener( configDesenhaArrasta );
+			
+			//bordinha enquanto desenha
+			this.addMouseMotionListener( configSinuetaDinamicaDesenho );
+
+
+			//menu popup
+			// build poup menu
+		    popup = new JPopupMenu();
+
+			JMenuItem m1,m2,m3,m4;
+			
+		    m1 = new JMenuItem("Selecionar video");
+		    m1.setMnemonic(KeyEvent.VK_V);
+		    
+		    m2 = new JMenuItem("Delete");
+		    m2.setMnemonic(KeyEvent.VK_D);
+		    
+		    m3 = new JMenuItem("Cortar");
+		    m3.setMnemonic(KeyEvent.VK_C);
+		    
+		    m4 = new JMenuItem("Inserir Texto nesta posicao");
+		    m4.setMnemonic(KeyEvent.VK_T);
+		    
+		    //cortar
+		    panel = new JPanel(new GridLayout(0, 1));
+		    panel2 = new JPanel(new GridLayout(0, 1));
+
+		    field1 = new JTextField("00:00:05.0");
+	        field2 = new JTextField("00:00:15.0");
+
+		    panel.add(new JLabel("tempo inicial no video original (HH:MM:SS.0)"));
+		    panel.add(field1);
+
+		    panel.add(new JLabel("tempo final no video original (HH:MM:SS.0)"));
+		    panel.add(field2);
+		    
+		    //texto
+		    panel2 = new JPanel(new GridLayout(0, 1));
+
+	        field5 = new JTextField("Texto");
+		    field3 = new JTextField("0");
+	        field4 = new JTextField("3");
+	        field6 = new JTextField("800x600");
+
+		    panel2.add(new JLabel("Dimensao do video selecionado que vai receber o texto"));
+		    panel2.add(field6);
+		    
+		    panel2.add(new JLabel("Texto escolhido"));
+		    panel2.add(field5);
+		    
+		    panel2.add(new JLabel("segundos de video necessarios para aparecer o texto"));
+		    panel2.add(field3);
+
+		    panel2.add(new JLabel("segundos de video necessarios para desaparecer o texto"));
+		    panel2.add(field4);
+
+		    m1.addActionListener(popupconfig1());
+		    m2.addActionListener(popupconfig2());
+		    m3.addActionListener(popupconfig3());
+		    m4.addActionListener(popupconfig4());
+		    	    
+		    popup.add(m1);
+		    popup.add(m2);	
+		    popup.add(m3);
+		    popup.add(m4);			    
+		    //fim menu popup
+		}
+		
+		//desenhar
+		public void desenharQuadradoColorido(int x1, int y1, int x2, int y2){
+			Polygon r = makeRectangle(x1, y1, x2, y2);
+			VideoBox r2 = new VideoBox(r,id++);
+			shapes.add(r2);
+			startDrag = null;
+			endDrag = null;
+			repaint();
+		}
+		
+		public void desenharTextoCentral(Graphics g, String text, Polygon rect, Font font) {
+			Paint old = tela.getPaint();
+			tela.setPaint(Color.BLACK);
+			
+		    FontMetrics metrics = g.getFontMetrics(font);
+
+		    int x = rect.xpoints[0] + (rect.xpoints[1]-rect.xpoints[0] - metrics.stringWidth(text)) / 2,
+		    	y = rect.ypoints[0] + ((rect.ypoints[3]-rect.ypoints[0] - metrics.getHeight()) / 2) + metrics.getAscent();
+		    
+		    g.setFont(font);
+		    g.drawString(text, x, y);
+		    
+			tela.setPaint(old);
+		}		
+		//fim desenhar
 		
 	//saveload
 		public void salvarTxt(String enderecoArquivo, String texto){
@@ -92,132 +216,218 @@ public class GUI extends JFrame {
 					int x1=Integer.parseInt(a[0]), y1=Integer.parseInt(a[1]), x2=x1+Integer.parseInt(a[2]), y2=y1+Integer.parseInt(a[3]);
 					
 					//desenhar na tela com os dados
-					Polygon r = makeRectangle(x1, y1, x2, y2);
-					VideoBox r2 = new VideoBox(r,id++);
-					shapes.add(r2);
-					repaint();
+					desenharQuadradoColorido(x1,y1,x2,y2);
 				}
 				in.close();
 			}catch(Exception ex){}
 		}
 	//fim saveload
+	
+	//popup config
+	public VideoBox videoAtualpopup;
+	public ActionListener popupconfig1(){
+			return new ActionListener() {
+		        public void actionPerformed(ActionEvent e) {
+		        	if(selecionarVideo.showOpenDialog(videoAtualpopup)==JFileChooser.APPROVE_OPTION){
+		        		videoAtualpopup.video=selecionarVideo.getSelectedFile().getAbsolutePath();
+						System.out.println(videoAtualpopup.video);
+					}
+		        }
+		    };
+	}	
+	public ActionListener popupconfig2(){
+		return new ActionListener() {
+	        public void actionPerformed(ActionEvent e) {
+	        	int idRemove=videoAtualpopup.id;
+	        	shapes.remove(videoAtualpopup);
+	        	
+	        	for(VideoBox s2:shapes){
+	        		if(s2.id>idRemove){
+	        			s2.id--;
+	        		}
+	        	}
+	        	id--;
+	        	repaint();
+	        	return;
+	        }
+	    };
+	}
+	
+	public ActionListener popupconfig3(){
+		return new ActionListener() {
+	        public void actionPerformed(ActionEvent e) {
+	        	if(videoAtualpopup.video==null){
+	        		JOptionPane.showMessageDialog(null, "Selecione um video antes para depois extrair um trecho do video escolhido", "Erro", JOptionPane.INFORMATION_MESSAGE);
+	        		return;
+	        	}else{
+	        		int result = JOptionPane.showConfirmDialog(null, panel, "Extrair intervalo",
+		        	            JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+        	        
+	        		if (result == JOptionPane.OK_OPTION) {
+        	        	IntegracaoBasicaFFmpeg.executarFFmpeg(IntegracaoBasicaFFmpeg.cortar(field1.getText(), field2.getText(), videoAtualpopup.video));
+        	        }
+	        	}	        	
+	        }
+	    };
+	}	
+	public ActionListener popupconfig4(){
+		return new ActionListener() {
+	        public void actionPerformed(ActionEvent e) {
+	        	if(videoAtualpopup.video==null){
+	        		JOptionPane.showMessageDialog(null, "Selecione um video antes para servir de base para inserir o texto", "Erro", JOptionPane.INFORMATION_MESSAGE);
+	        		for(int i=0; i<videoAtualpopup.shape.ypoints.length;i++){
+	        			System.out.println(videoAtualpopup.shape.ypoints[i]+" ");
+	        		}
+	        		videoAtualpopup.shape.ypoints[3]=videoAtualpopup.shape.ypoints[2]=(int) (videoAtualpopup.shape.ypoints[0]  + (0.8 * IntegracaoBasicaFFmpeg.tamanhoFonte));
+	        		repaint();
+	        		return;
+	        	}else{
+	        		KeyboardFocusManager.getCurrentKeyboardFocusManager().removeKeyEventDispatcher(configTeclado);
+	        		int result = JOptionPane.showConfirmDialog(null, panel2, "Texto",
+		        	            JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+	    	        
+	        		if (result == JOptionPane.OK_OPTION) {
+	        			String[] dimensao = field6.getText().split("x");
+	        			int x = videoAtualpopup.shape.xpoints[0] * Integer.parseInt(dimensao[0])/640,
+	        					y = videoAtualpopup.shape.ypoints[0] * Integer.parseInt(dimensao[1])/480;
+
+	        			System.out.println("executei texto");
+	    	        	IntegracaoBasicaFFmpeg.executarFFmpeg(IntegracaoBasicaFFmpeg.addText(
+		    	        			field5.getText(), 
+		    	        			x, y, 
+		    	        			Integer.parseInt(field3.getText()), Integer.parseInt(field4.getText()), videoAtualpopup.video) 
+	    	        			);
+	    	        } 
+
+	        		KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(configTeclado);
+	        	}
+	        }
+	    };
+	}		
+	//fim popup config
 		
 	//configs
-		public KeyEventDispatcher configTeclado(){
-			return new KeyEventDispatcher() {
-				@Override
-				public boolean dispatchKeyEvent(KeyEvent e) {
-					synchronized (PaintSurface.class) {
-						if(e.getID()==KeyEvent.KEY_RELEASED){						
-							switch(e.getKeyCode()){
-								case ('s'-32):
-									String texto="";
-									for(VideoBox s : shapes) texto+=s.toString()+"\r\n";
-									
-									salvarTxt(getDesktop()+"\\salvar.txt",texto);
-								break;
-								
-								case ('l'-32):
-									System.out.println("load");
-									loadTxt(getDesktop()+"\\salvar.txt");
-								break;							
-							}						
-						}
-						return false;
-					}
-				}
-			};
-		}
-		
-		public MouseAdapter configDesenhaArrasta(){
-			return new MouseAdapter() {
-				public void mousePressed(MouseEvent e) {
-					wantDraw=true;
-					for(VideoBox s : shapes) {
-						if (s.shape.contains(e.getPoint())) {//check if mouse is clicked within shape
-							wantDraw=false;
-							System.out.println("Clicked a "+s.id);
-							dragged = s.shape;
-							lastLocation = e.getPoint();
+	//config teclado
+	final KeyEventDispatcher configTeclado = new KeyEventDispatcher() {
+		@Override
+		public boolean dispatchKeyEvent(KeyEvent e) {
+			synchronized (PaintSurface.class) {
+				if(e.getID()==KeyEvent.KEY_RELEASED){						
+					switch(e.getKeyCode()){
+						case ('s'-32):
+							String texto="";
+							for(VideoBox s : shapes) texto+=s.toString()+"\r\n";
 							
-							if(e.getButton()==e.BUTTON3){
-								if(selecionarVideo.showOpenDialog(s)==JFileChooser.APPROVE_OPTION){
-									s.video=selecionarVideo.getSelectedFile().getAbsolutePath();
-									System.out.println(s.video);
-								}
-							}							
-							return;//ja entendi o q ele quer nao preciso ver os outros, sair
-						}
-					}
-					
-					if(wantDraw){
-						startDrag = new Point(e.getX(), e.getY());
-						endDrag = startDrag;
-					}
+							salvarTxt(getDesktop()+"\\salvar.txt",texto);
+						break;
+						
+						case ('l'-32):
+							System.out.println("load");
+							loadTxt(getDesktop()+"\\salvar.txt");
+						break;
+						
+						case ('r'-32):
+							System.out.println();
+							String[] enderecoVideos = new String[id-1];
+							int i=0;
+							int tamanho= shapes.size();
+							int[] xpoints = new int[tamanho], ypoints= new int[tamanho],x2points = new int[tamanho], y2points= new int[tamanho];
+							for(VideoBox s: shapes){
+								enderecoVideos[i]=s.video;
+								xpoints[i]=s.shape.xpoints[0] * 1920/640;
+								ypoints[i]=s.shape.ypoints[0] * 1080/480;
+								
+								x2points[i]=s.shape.xpoints[1] * 1920/640;
+								y2points[i]=s.shape.ypoints[3] * 1080/480;
+								i++;
+							}
+//							String[] b = IntegracaoBasicaFFmpeg.mosaic(enderecoVideos,xpoints,ypoints);
+//							System.out.println(b[b.length-1]);
+							IntegracaoBasicaFFmpeg.executarFFmpeg(IntegracaoBasicaFFmpeg.mosaic(enderecoVideos,xpoints,ypoints,x2points,y2points));
+							break;
+					}						
 				}
-				
-				public void mouseReleased(MouseEvent e) {
-					if(wantDraw){
-						Polygon r = makeRectangle(startDrag.x, startDrag.y, e.getX(), e.getY());
-						VideoBox r2 = new VideoBox(r,id++);
-						shapes.add(r2);
-						startDrag = null;
-						endDrag = null;
-						repaint();
-					}
-				}
-			};
+				return false;
+			}
 		}
+	};
+	//fim configteclado
 	
-		public MouseMotionAdapter configSinuetaDinamicaDesenho(){
-			return new MouseMotionAdapter() {
-				public void mouseDragged(MouseEvent e) {
-					if(wantDraw){
-						endDrag = new Point(e.getX(), e.getY());
-						repaint();
-					}else{
-						dragged.translate(e.getX() - lastLocation.x, e.getY() - lastLocation.y);
-						lastLocation = e.getPoint();
-						repaint();
-					}
+	//config desenha arrasta
+	final MouseAdapter configDesenhaArrasta = new MouseAdapter() {
+		public void mousePressed(MouseEvent e) {
+			wantDraw=true;
+			for(VideoBox s : shapes) {
+				if (s.shape.contains(e.getPoint())) {//check if mouse is clicked within shape
+					wantDraw=false;
+					System.out.println("Clicked a "+s.id);
+					dragged = s.shape;
+					lastLocation = e.getPoint();
+					
+					if(e.getButton()==e.BUTTON3){
+						videoAtualpopup=s;
+						popup.show(e.getComponent(), e.getX(), e.getY());
+					}			
+					return;//ja entendi o q ele quer nao preciso ver os outros, sair
 				}
-			};
+			}
+			
+			if(wantDraw){
+				startDrag = new Point(e.getX(), e.getY());
+				endDrag = startDrag;
+			}
 		}
-	//fim configs
 		
-		public PaintSurface() {
-			selecionarVideo.setCurrentDirectory(workingDirectory);
-			
-			//salvar ou load usando teclado
-			KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher( configTeclado()	);
-			
-			//config desenhaArrasta
-			this.addMouseListener( configDesenhaArrasta() );
-			
-			//bordinha enquanto desenha
-			this.addMouseMotionListener( configSinuetaDinamicaDesenho() );
+		public void mouseReleased(MouseEvent e) {
+			if(startDrag==endDrag)return;
+			if(wantDraw)
+				desenharQuadradoColorido(startDrag.x, startDrag.y, e.getX(), e.getY());
 		}
+	};
+	//fim config desenha arrasta
+	
+	final MouseMotionAdapter configSinuetaDinamicaDesenho = new MouseMotionAdapter() {
+			public void mouseDragged(MouseEvent e) {
+				if(wantDraw){
+					endDrag = new Point(e.getX(), e.getY());
+					repaint();
+				}else{
+					dragged.translate(e.getX() - lastLocation.x, e.getY() - lastLocation.y);
+					lastLocation = e.getPoint();
+					repaint();
+				}
+			}
+		};
+	//fim configs
 		
 	//desenha na tela ( repaint() usa esse metodo)
 		public void paint(Graphics g) {
 			tela = (Graphics2D) g;
 			tela.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 			
-			Color[] colors = { Color.YELLOW, Color.MAGENTA, Color.CYAN , Color.RED, Color.BLUE};
+			Color[] colors = { Color.YELLOW, Color.MAGENTA, Color.CYAN , Color.RED /*, Color.BLUE*/};
 			int qtdCores=colors.length;
 			int colorIndex = 0;
 	
 			tela.setStroke(new BasicStroke(2));
 			tela.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.50f));
-	
+			
+			//pintar video base
+
+			tela.setPaint(Color.RED);
+			tela.draw(makeRectangle(0, 0, 600, 480));
+			
+			
 			for (VideoBox s : shapes) {
 				tela.setPaint(Color.BLACK);
 				tela.draw(s.shape);
 				tela.setPaint(colors[(colorIndex++) % qtdCores]);
 				tela.fill(s.shape);
+				
+				desenharTextoCentral(g, s.id+"", s.shape, new Font("Arial", 0, 18));
 			}
 	
-			if (startDrag != null && endDrag != null) {
+			if (startDrag != null && endDrag != null) { //pintar contornado
 				tela.setPaint(Color.LIGHT_GRAY);
 				Polygon r = makeRectangle(startDrag.x, startDrag.y, endDrag.x, endDrag.y);
 				tela.draw(r);
@@ -238,5 +448,5 @@ public class GUI extends JFrame {
 			return RectangleToPolygon( new Rectangle2D.Float(Math.min(x1, x2), Math.min(y1, y2), Math.abs(x1 - x2), Math.abs(y1 - y2)) );
 		}
 	//fim poligono
-	}
+	}	
 }
--- src/IntegracaoBasicaFFmpeg.java
+++ src/IntegracaoBasicaFFmpeg.java
@@ -3,29 +3,40 @@ import java.util.LinkedList;
 public class IntegracaoBasicaFFmpeg {
 	
 	final static String[] verbose = {"-loglevel","panic"};
-	final static String pastaRaiz= "C:\\Users\\rslgp\\Downloads\\multimidia-master\\src\\";
+	final static String pastaRaiz= IntegracaoBasicaFFmpeg.class.getProtectionDomain().getCodeSource().getLocation().getPath().substring(1).replace('/', '\\');
 	final static String ffmpeg = pastaRaiz+"ffmpeg\\bin\\ffmpeg.exe";
 	final static String enderecoArquivoVideo = pastaRaiz+"ffmpeg\\videoExemplo\\";
 	final static String enderecoArquivoFonteTexto = pastaRaiz.charAt(0)+"\\\\"+pastaRaiz.substring(1).replace('\\', '/')+"ffmpeg/tutorial";
 	
 	static Process processoFFmpeg;
 	
-	public static void executarFFmpeg(String[] parametros) throws Exception{		
-		processoFFmpeg = Runtime.getRuntime().exec(parametros);
+	static int tamanhoFonte=40;
+	
+	public static void executarFFmpeg(String[] parametros){		
+		try {
+			processoFFmpeg = Runtime.getRuntime().exec(parametros);
+			processoFFmpeg.waitFor();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		processoFFmpeg.waitFor();
 	}
 	
+	public static String getExtensaoVideo(String enderecoVideo){ return enderecoVideo.substring(enderecoVideo.lastIndexOf('.'));}
+	
 	/*
-	 * para mostrar o progresso do ffmpeg, coloca antes do parametro ffmpeg 
+	 * para mostrar o progresso do ffmpeg (tirar o verbose), coloca antes do parametro ffmpeg 
 //				"cmd",
 //				"/c",
 //				"start",
 	 * */
 	
 	public static String[] padraoParametros(String input, String output, String[] core){
-		String arquivoEntrada = enderecoArquivoVideo+input,
-				arquivoSaida = enderecoArquivoVideo+output;
+//		String arquivoEntrada = enderecoArquivoVideo+input,
+//				arquivoSaida = enderecoArquivoVideo+output;
+		String arquivoEntrada = input,
+				arquivoSaida = output;
 		
 		String[] padrao={
 //			"cmd",
@@ -62,14 +73,16 @@ public class IntegracaoBasicaFFmpeg {
 			parameters[indexParametros++]=core[i];
 		}
 		
-//		imprimirParametros(parameters);
+		imprimirParametros(parameters);
 		
 		return parameters;		
 	}
 
-	public static String[] cortar(String inicio, String fim) throws Exception{
-		String input = "video.mp4",
-				output = "split.mp4";
+	public static String[] cortar(String inicio, String fim, String enderecoVideo){
+//		String input = "video.mp4",
+//				output = "split.mp4";
+		String input = enderecoVideo,
+				output = enderecoVideo.substring(0,enderecoVideo.lastIndexOf('\\'))+"\\split"+getExtensaoVideo(enderecoVideo);
 		
 		String[] core={
 			"-ss",
@@ -83,30 +96,42 @@ public class IntegracaoBasicaFFmpeg {
 		return padraoParametros(input, output, core);
 	}
 	
-	public static String[] makeVideoResizedBorder() throws Exception{
-		String input = "split.mp4",
-				output = "out.mp4";
-		
-		String[] core={
-			"-filter_complex",
-			"\"scale=578:462,pad=720:576:71:57\""				
-		};		
-		
-		return padraoParametros(input, output, core);		
-	}
+//	public static String[] makeVideoResizedBorder(){
+//		String input = "split.mp4",
+//				output = "out.mp4";
+//		
+//		String[] core={
+//			"-filter_complex",
+//			"\"scale=578:462,pad=720:576:71:57\""				
+//		};		
+//		
+//		return padraoParametros(input, output, core);		
+//	}
 	
-	public static String getScreenMosaic(int larguraScreen, int alturaScreen, int larguraPip, int alturaPip, int espacamentoAlturaEntrePip){
-		String nome0="[base]",nome1="[upperright]",nome2="[upperright]",nome3="[lowerright]";
-		String background= "\"color=c=black:size="+larguraScreen+"x"+alturaScreen+" "+nome0+"; ";
-		String colagem =  "overlay=shortest=1:x="+(larguraScreen-larguraPip);
-		String videoPadrao=":v] setpts=PTS-STARTPTS, scale="+larguraPip+"x"+alturaPip;
+	public static String getScreenMosaic(int larguraScreen, int alturaScreen, int larguraPip, int alturaPip, int espacamentoAlturaEntrePip, int[] xpoints, int[] ypoints, int[] x2points, int[] y2points){
+//		String nome0="[base]",nome1="[upperright]",nome2="[upperright]",nome3="[lowerright]";
+//		String background= "\"color=c=black:size="+larguraScreen+"x"+alturaScreen+" "+nome0+"; ";
+//		String colagem =  "overlay=shortest=1:x=" /* +(larguraScreen-larguraPip)*/;
+//		String videoPadrao=":v] setpts=PTS-STARTPTS, scale="+larguraPip+"x"+alturaPip;
+//		
+//		String final1=" [tmp1]", final2="[tmp2]";
+//		
+//		String retorno= background+"[0"+videoPadrao+nome1+"; [1"+videoPadrao+nome2+"; [2"+videoPadrao+nome3+"; "
+//		+nome0+nome1+colagem+xpoints[0]+":y="+ypoints[0]+final1+";"
+//		+final1+nome2+colagem+xpoints[1]+":y="+ypoints[1]+final2+";"
+//		+final2+nome3+colagem+xpoints[2]+":y="+ypoints[2]+"\"";
 		
-		String final1="[tmp1]", final2="[tmp2]";
 		
-		String retorno= background+"[0"+videoPadrao+nome1+"; [1"+videoPadrao+nome2+"; [2"+videoPadrao+nome3+"; "
-		+nome0+nome1+colagem+" "+final1+";"
-		+final1+nome2+colagem+":y="+(alturaPip+espacamentoAlturaEntrePip)*1+final2+";"
-		+final2+nome3+colagem+":y="+(alturaPip+espacamentoAlturaEntrePip)*2+"\"";
+		int qtdVideos=xpoints.length;
+		String retorno="\"color=c=black:size="+larguraScreen+"x"+alturaScreen+" [base]; ";
+		for(int i=0; i<qtdVideos;i++){
+			retorno+="["+i+":v] setpts=PTS-STARTPTS, scale="+ (x2points[i]-xpoints[i])+"x"+(y2points[i]-ypoints[i])+"["+(char)(i+'a')+"]; ";
+		}
+		retorno+="[base][a]overlay=shortest=1:x="+xpoints[0]+":y="+ypoints[0]+" [y];";
+		for(int i=1; i<qtdVideos;i++){
+			retorno+="["+(char)('z'-i)+"]["+(char)(i+'a')+"]overlay=shortest=1:x="+xpoints[i]+":y="+ypoints[i]+"  ["+(char)('z'-(i+1))+"];";
+		}
+		retorno = retorno.substring(0,retorno.length()- 6)+"\"";
 				
 //		 "[0:v] setpts=PTS-STARTPTS, scale=599x336[upperright];"
 //		 "[1:v] setpts=PTS-STARTPTS, scale=599x336 [upperright];"
@@ -118,37 +143,68 @@ public class IntegracaoBasicaFFmpeg {
 		return retorno;
 	}
 	
-	public static String[] mosaic() throws Exception{
-		String input = "split.mp4",
-				input2 = "split.mp4",
-				input3 ="split.mp4",
-				output = "out.mp4";
+	public static String[] mosaic(String[] enderecoVideos, int[] xpoints, int[] ypoints,int[] x2points, int[] y2points){
+//		public static String[] mosaic(){
+//		String input = "split.mp4",
+//				input2 = enderecoArquivoVideo+"split.mp4",
+//				input3 =enderecoArquivoVideo+"split.mp4",
+//				output = "mosaic.mp4";
+		String input = enderecoVideos[0],
+				output=enderecoVideos[0].substring(0, enderecoVideos[0].lastIndexOf('\\'))+"\\mosaic"+getExtensaoVideo(enderecoVideos[0]);
+
 		
-		String[] core={
-				"-i",
-				input2,
-				"-i",
-				input3,
+		String[] coreMosaic={
 				"-filter_complex",
-				getScreenMosaic(1920,1080,599,336,2),
+				getScreenMosaic(1920,1080,599,336,2,xpoints,ypoints,x2points,y2points),
 				"-c:v",
 				"libx264"				
 		};
+//		String[] core={
+//				"-i",
+//				input2,
+//				"-i",
+//				input3,
+//				"-filter_complex",
+//				getScreenMosaic(1920,1080,599,336,2),
+//				"-c:v",
+//				"libx264"				
+//		};
+		
+		int tamanho1=enderecoVideos.length,
+				tamanho2=coreMosaic.length;
+		
+		int novoTamanho1=tamanho1*2;
+		
+		String[] core = new String[novoTamanho1 + tamanho2 -2];
+		
+		int i=0;
+		for(int j=1; j<tamanho1;i+=2, j++){
+			core[i]="-i";
+			core[i+1]=enderecoVideos[j];
+		}
+		
+		for(int j=0; j<tamanho2;i++, j++){
+			core[i]=coreMosaic[j];
+		}
 		
 		return padraoParametros(input, output, core);
 	}
 	
-	public static String[] addText(String texto) throws Exception{		
-		String input = "out.mp4",
-				output = "text.mp4",
-				fonte="\""+enderecoArquivoFonteTexto+"/HelveticaNeueLTStd-Th_1.otf\"",
-				time="enable='between(t,0,3)'";
+	public static String[] addText(String texto, int x, int y, int duracaoInicio, int duracaoFinal, String enderecoVideo){		
+//		String input = "out.mp4",
+//				output = "text.mp4",
+
+		String input = enderecoVideo,
+				output=enderecoVideo.substring(0, enderecoVideo.lastIndexOf('\\'))+"\\texto"+getExtensaoVideo(enderecoVideo);
+		
+		String fonte="\""+enderecoArquivoFonteTexto+"/HelveticaNeueLTStd-Th_1.otf\"",
+		time="enable='between(t,"+duracaoInicio+","+duracaoFinal+")'";
 		
 		String[] core={
 				"-vf",
-				"drawtext="+time+":fontfile="+fonte+":text=\'"+texto+"\':fontcolor=white:fontsize=40:x=0:y=0",
+				"drawtext="+time+":fontfile="+fonte+":text=\'"+texto+"\':fontcolor=white:fontsize="+tamanhoFonte+":x="+x+":y="+y,
 				"-codec:a",
-				"copy",
+				"copy"
 		};
 		
 //		System.out.println("\n"+processoFFmpeg.getErrorStream());
@@ -174,21 +230,21 @@ public class IntegracaoBasicaFFmpeg {
 	}
 	static LinkedList<String[]> comandos;
 	
-	public static void main(String[] args) {
-//		String arquivo= JOptionPane.showInputDialog("insira endereco do arquivo: ");
-		comandos = new LinkedList<String[]>();
-		try {
-			comandos.add(cortar("00:00:05.0","00:00:15.0"));
-			//comandos.add(girarVideo());
-			comandos.add(makeVideoResizedBorder());
-			comandos.add(mosaic());
-			comandos.add(addText("teste"));
-			
-			for(String[] parametros : comandos) executarFFmpeg(parametros);
-			
-			System.out.println("veja a pasta "+enderecoArquivoVideo);
-			
-		} catch (Exception e){e.printStackTrace();}
-		
-	}
+//	public static void main(String[] args) {
+////		String arquivo= JOptionPane.showInputDialog("insira endereco do arquivo: ");
+//		comandos = new LinkedList<String[]>();
+//		try {
+////			comandos.add(cortar("00:00:05.0","00:00:15.0"));
+//			//comandos.add(girarVideo());
+////			comandos.add(makeVideoResizedBorder());
+//			comandos.add(mosaic());
+////			comandos.add(addText("teste"));
+//			
+//			for(String[] parametros : comandos) executarFFmpeg(parametros);
+//			
+//			System.out.println("veja a pasta "+enderecoArquivoVideo);
+//			
+//		} catch (Exception e){e.printStackTrace();}
+//		
+//	}
 }
