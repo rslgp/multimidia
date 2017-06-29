ed2b5178c8d41984df6eb9151dc829d36fa7c12b
rslgp <rslgp@cin.ufpe.br>
2017-06-29 00:30:44 -0300

    lookandfeel SO em uso

--- src/GUINOVA.java
+++ src/GUINOVA.java
@@ -1,25 +1,27 @@
-import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.UIManager;
 import javax.swing.border.EmptyBorder;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 
 public class GUINOVA extends JFrame {
 	private JPanel contentPane;
 	private DrawPanel drawPanel;
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
+					//usar tema atual do windows
+					UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+					
 					GUINOVA frame = new GUINOVA();
 					frame.setVisible(true);
+					frame.setTitle("Orion Multi-Video Editor");
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -46,17 +48,17 @@ public class GUINOVA extends JFrame {
 		
 		JButton botaoSalvar = new JButton("Salvar");
 		botaoSalvar.addActionListener(configAcao('s'));
-		botaoSalvar.setBounds(10, 11, 90, 23);
+		botaoSalvar.setBounds(10, 10, 90, 23);
 		contentPane.add(botaoSalvar);
 		
 		JButton botaoLoad = new JButton("Load");
 		botaoLoad.addActionListener(configAcao('l'));
-		botaoLoad.setBounds(10, 61, 90, 23);
+		botaoLoad.setBounds(10, 50, 90, 23);
 		contentPane.add(botaoLoad);
 		
 		JButton botaoRender = new JButton("Render");
 		botaoRender.addActionListener(configAcao('r'));
-		botaoRender.setBounds(10, 106, 89, 23);
+		botaoRender.setBounds(10, 90, 90, 23);
 		contentPane.add(botaoRender);		
 		
 		//rangeslider classe component opcional para cortar https://github.com/ernieyu/Swing-range-slider/tree/master/src/slider
--- src/IntegracaoBasicaFFmpeg.java
+++ src/IntegracaoBasicaFFmpeg.java
@@ -1,6 +1,3 @@
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.LinkedList;
 
 public class IntegracaoBasicaFFmpeg {
