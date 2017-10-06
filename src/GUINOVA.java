eab664ac7527f094f5dbc909dd8eb48f5e36109e
rslgp <rslgp@cin.ufpe.br>
2017-10-06 00:15:39 -0300

    Update GUINOVA.java

--- src/GUINOVA.java
+++ src/GUINOVA.java
@@ -4,7 +4,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.text.DecimalFormat;
-
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -20,191 +19,211 @@ import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
 import javax.swing.text.NumberFormatter;
 
-public class GUINOVA extends JFrame {
-	private JPanel contentPane;
-	private DrawPanel drawPanel;
-	private JComboBox resolucoesList;
-	private JFormattedTextField customBase,customAltura, customBaseEdit,customAlturaEdit;
-	private NumberFormatter formatter;
-	private int base,altura;
-	
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					//usar tema atual do windows
-					UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-					
-					GUINOVA frame = new GUINOVA();
-					frame.setVisible(true);
-					frame.setTitle("Orion Multi-Video Editor");
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-	private ActionListener configAcao(char acao){
-		return new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				drawPanel.executarAcao(acao);
-			}
-		};
-	} 
-	
-	private ActionListener configResolucao(){
-		return new ActionListener() {//add actionlistner to listen for change
-		    @Override
-		    public void actionPerformed(ActionEvent e) {
-
-		        String s = (String) resolucoesList.getSelectedItem();//get the selected item
-
-		        switch (s) {//check for a match
-		            case "1080p":
-		        		customBase.setText("1920");
-		        		customAltura.setText("1080");
-		            	VariavelGlobal.resolucaoWidthVideoOutput=1920;
-		            	VariavelGlobal.resolucaoHeightVideoOutput=1080;
-		                break;
-		            case "720p":
-		        		customBase.setText("1280");
-		        		customAltura.setText("720");
-		            	VariavelGlobal.resolucaoWidthVideoOutput=1280;
-		            	VariavelGlobal.resolucaoHeightVideoOutput=720;
-		                break;
-//		            case "480p":
-//	        			customBase.setText("854");
-//		        		customAltura.setText("480");
-//		            	VariavelGlobal.resolucaoWidthVideoOutput=854;
-//		            	VariavelGlobal.resolucaoHeightVideoOutput=480;
-//		                break;
-		            case "360p":
-	        			customBase.setText("640");
-		        		customAltura.setText("360");
-		            	VariavelGlobal.resolucaoWidthVideoOutput=640;
-		            	VariavelGlobal.resolucaoHeightVideoOutput=360;
-		                break;
-
-		            case "Custom":
-		        		Object[] message = {
-		        		    "base:", customBaseEdit,
-		        		    "altura:", customAlturaEdit
-		        		};
-
-		        		int option = JOptionPane.showConfirmDialog(null, message, "Login", JOptionPane.OK_CANCEL_OPTION);
-		        		if (option == JOptionPane.OK_OPTION) {
-		        			base = (int)customBaseEdit.getValue();
-		        			altura = (int)customAlturaEdit.getValue();
-		        			
-		        			customBase.setText(base+"");
-			        		customAltura.setText(altura+"");
-			            	VariavelGlobal.resolucaoWidthVideoOutput=base;
-			            	VariavelGlobal.resolucaoHeightVideoOutput=altura;	        		   
-		        		}
-
-		            	customBase.setEnabled(false);
-		            	customAltura.setEnabled(false);
-		                break;
-		        }
-		    }
-		};
-	}	
-
-	private final JFileChooser selecionarImg = new JFileChooser();
-	private final File workingDirectory = new File(System.getProperty("user.dir"));
-	
-	public GUINOVA() {
-
-		selecionarImg.setCurrentDirectory(workingDirectory);
-		
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 800, 430);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(null);
-		drawPanel = new DrawPanel();
-		drawPanel.setLocation(130, 13);
-		drawPanel.setBackground(new Color(232, 232, 232));
-		contentPane.add(drawPanel);
-		setContentPane(contentPane);
-		
-		JButton botaoSalvar = new JButton("Salvar");
-		botaoSalvar.addActionListener(configAcao('s'));
-		botaoSalvar.setBounds(10, 10, 90, 23);
-		contentPane.add(botaoSalvar);
-		
-		JButton botaoLoad = new JButton("Load");
-		botaoLoad.addActionListener(configAcao('l'));
-		botaoLoad.setBounds(10, 50, 90, 23);
-		contentPane.add(botaoLoad);
-		
-		JButton botaoRender = new JButton("Render");
-		botaoRender.addActionListener(configAcao('r'));
-		botaoRender.setBounds(10, 90, 90, 23);
-		contentPane.add(botaoRender);		
-
-		//selecionar imagem referencia
-		JButton botaoBackground = new JButton("<html>Imagem de<br>Refer\u00EAncia");
-		botaoBackground.addActionListener(configAcao('b'));
-		botaoBackground.setBounds(10, 130, 109, 43);
-		contentPane.add(botaoBackground);	
-		//fim imagem referencia
-		
-
-		JButton botaoFullscreen = new JButton("<html>Video on<br>Fullscreen");
-		botaoFullscreen.addActionListener(configAcao('f'));
-		botaoFullscreen.setBounds(10, 191, 109, 43);
-		contentPane.add(botaoFullscreen);
-		
-		//configurar selecao de resolucoes
-		
-		String[] resolucoes = new String[] {"1080p", "720p","360p"};
-		DefaultComboBoxModel<String> comboModel = new DefaultComboBoxModel<String>(resolucoes);		
-		JPanel panel = new JPanel();
-		panel.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null), "<html>Resolu\u00E7\u00E3o do<br>v\u00EDdeo resultado", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
-		panel.setBounds(10, 269, 102, 123);
-		contentPane.add(panel);
-		panel.setLayout(null);
-		resolucoesList = new JComboBox(comboModel);
-		resolucoesList.setBounds(10, 37, 90, 23);
-		panel.add(resolucoesList);	
-
-
-	    DecimalFormat format = new DecimalFormat();
-	    format.setGroupingUsed(false);
-	    formatter = new NumberFormatter(format);
-	    //formatter.setValueClass(Integer.class);
-	    formatter.setMinimum(0);
-	    formatter.setMaximum(Integer.MAX_VALUE);
-	    formatter.setAllowsInvalid(false);
-	    // If you want the value to be committed on each keystroke instead of focus lost
-	    formatter.setCommitsOnValidEdit(true);
-	    
-		customBase = new JFormattedTextField(formatter);
-		customBase.setEnabled(false);
-		customBase.setToolTipText("base");
-		customBase.setBounds(10, 71, 35, 20);
-		panel.add(customBase);
-		
-		customAltura = new JFormattedTextField(formatter);
-		customAltura.setEnabled(false);
-		customAltura.setToolTipText("altura");
-		customAltura.setBounds(50, 71, 42, 20);
-		panel.add(customAltura);
-		
-		customBase.setText("1920");
-		customAltura.setText("1080");
-		
-		JLabel label = new JLabel("16:9");
-		label.setBounds(10, 98, 46, 14);
-		panel.add(label);
-		
-    	customBaseEdit= new JFormattedTextField(formatter);
-    	customAlturaEdit= new JFormattedTextField(formatter);
-		
-		resolucoesList.addActionListener(configResolucao());
-		//fim config resolucoes
-		
-		//rangeslider classe component opcional para cortar https://github.com/ernieyu/Swing-range-slider/tree/master/src/slider
-	}
+public class GUINOVA
+  extends JFrame
+{
+  private JPanel contentPane;
+  private DrawPanel drawPanel;
+  private JComboBox resolucoesList;
+  private JFormattedTextField customBase;
+  private JFormattedTextField customAltura;
+  private JFormattedTextField customBaseEdit;
+  private JFormattedTextField customAlturaEdit;
+  private NumberFormatter formatter;
+  private int base;
+  private int altura;
+  
+  public static void main(String[] args)
+  {
+    EventQueue.invokeLater(new Runnable()
+    {
+      public void run()
+      {
+        try
+        {
+          UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+          
+          GUINOVA frame = new GUINOVA();
+          frame.setVisible(true);
+          frame.setTitle("Orion Multi-Video Editor");
+        }
+        catch (Exception e)
+        {
+          e.printStackTrace();
+        }
+      }
+    });
+  }
+  
+  private ActionListener configAcao(final char acao)
+  {
+    new ActionListener()
+    {
+      public void actionPerformed(ActionEvent arg0)
+      {
+        GUINOVA.this.drawPanel.executarAcao(acao);
+      }
+    };
+  }
+  
+  private ActionListener configResolucao()
+  {
+    new ActionListener()
+    {
+      public void actionPerformed(ActionEvent e)
+      {
+        String s = (String)GUINOVA.this.resolucoesList.getSelectedItem();
+        String str1;
+        switch ((str1 = s).hashCode())
+        {
+        case 1572835: 
+          if (str1.equals("360p")) {}
+          break;
+        case 1688155: 
+          if (str1.equals("720p")) {}
+          break;
+        case 46737913: 
+          if (str1.equals("1080p")) {
+            break;
+          }
+          break;
+        case 2029746065: 
+          if (!str1.equals("Custom"))
+          {
+            return;GUINOVA.this.customBase.setText("1920");
+            GUINOVA.this.customAltura.setText("1080");
+            VariavelGlobal.resolucaoWidthVideoOutput = 1920;
+            VariavelGlobal.resolucaoHeightVideoOutput = 1080;
+            return;
+            
+            GUINOVA.this.customBase.setText("1280");
+            GUINOVA.this.customAltura.setText("720");
+            VariavelGlobal.resolucaoWidthVideoOutput = 1280;
+            VariavelGlobal.resolucaoHeightVideoOutput = 720;
+            return;
+            
+            GUINOVA.this.customBase.setText("640");
+            GUINOVA.this.customAltura.setText("360");
+            VariavelGlobal.resolucaoWidthVideoOutput = 640;
+            VariavelGlobal.resolucaoHeightVideoOutput = 360;
+          }
+          else
+          {
+            Object[] message = {
+              "base:", GUINOVA.this.customBaseEdit, 
+              "altura:", GUINOVA.this.customAlturaEdit };
+            
+            int option = JOptionPane.showConfirmDialog(null, message, "Login", 2);
+            if (option == 0)
+            {
+              GUINOVA.this.base = ((Integer)GUINOVA.this.customBaseEdit.getValue()).intValue();
+              GUINOVA.this.altura = ((Integer)GUINOVA.this.customAlturaEdit.getValue()).intValue();
+              
+              GUINOVA.this.customBase.setText(GUINOVA.this.base);
+              GUINOVA.this.customAltura.setText(GUINOVA.this.altura);
+              VariavelGlobal.resolucaoWidthVideoOutput = GUINOVA.this.base;
+              VariavelGlobal.resolucaoHeightVideoOutput = GUINOVA.this.altura;
+            }
+            GUINOVA.this.customBase.setEnabled(false);
+            GUINOVA.this.customAltura.setEnabled(false);
+          }
+          break;
+        }
+      }
+    };
+  }
+  
+  private final JFileChooser selecionarImg = new JFileChooser();
+  private final File workingDirectory = new File(System.getProperty("user.dir"));
+  
+  public GUINOVA()
+  {
+    this.selecionarImg.setCurrentDirectory(this.workingDirectory);
+    
+    setDefaultCloseOperation(3);
+    setBounds(100, 100, 800, 485);
+    this.contentPane = new JPanel();
+    this.contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+    this.contentPane.setLayout(null);
+    this.drawPanel = new DrawPanel();
+    this.drawPanel.setLocation(130, 13);
+    this.drawPanel.setBackground(new Color(232, 232, 232));
+    this.contentPane.add(this.drawPanel);
+    setContentPane(this.contentPane);
+    
+    JButton botaoSalvar = new JButton("Salvar");
+    botaoSalvar.addActionListener(configAcao('s'));
+    botaoSalvar.setBounds(10, 10, 90, 23);
+    this.contentPane.add(botaoSalvar);
+    
+    JButton botaoLoad = new JButton("Load");
+    botaoLoad.addActionListener(configAcao('l'));
+    botaoLoad.setBounds(10, 50, 90, 23);
+    this.contentPane.add(botaoLoad);
+    
+    JButton botaoRender = new JButton("Render");
+    botaoRender.addActionListener(configAcao('r'));
+    botaoRender.setBounds(10, 90, 90, 23);
+    this.contentPane.add(botaoRender);
+    
+    JButton gifItButton = new JButton("Gif it!");
+    gifItButton.addActionListener(configAcao('g'));
+    gifItButton.setBounds(10, 390, 91, 23);
+    this.contentPane.add(gifItButton);
+    
+    JButton botaoBackground = new JButton("<html>Imagem de<br>Refer�ncia");
+    botaoBackground.addActionListener(configAcao('b'));
+    botaoBackground.setBounds(10, 130, 109, 43);
+    this.contentPane.add(botaoBackground);
+    
+    JButton botaoFullscreen = new JButton("<html>Video on<br>Fullscreen");
+    botaoFullscreen.addActionListener(configAcao('f'));
+    botaoFullscreen.setBounds(10, 184, 109, 43);
+    this.contentPane.add(botaoFullscreen);
+    
+    String[] resolucoes = { "1080p", "720p", "360p" };
+    DefaultComboBoxModel<String> comboModel = new DefaultComboBoxModel(resolucoes);
+    JPanel panel = new JPanel();
+    panel.setBorder(new TitledBorder(new EtchedBorder(1, null, null), "<html>Resolu��o do<br>v�deo resultado", 4, 2, null, new Color(0, 0, 0)));
+    panel.setBounds(10, 245, 102, 123);
+    this.contentPane.add(panel);
+    panel.setLayout(null);
+    this.resolucoesList = new JComboBox(comboModel);
+    this.resolucoesList.setBounds(10, 37, 90, 23);
+    panel.add(this.resolucoesList);
+    
+    DecimalFormat format = new DecimalFormat();
+    format.setGroupingUsed(false);
+    this.formatter = new NumberFormatter(format);
+    
+    this.formatter.setMinimum(Integer.valueOf(0));
+    this.formatter.setMaximum(Integer.valueOf(Integer.MAX_VALUE));
+    this.formatter.setAllowsInvalid(false);
+    
+    this.formatter.setCommitsOnValidEdit(true);
+    
+    this.customBase = new JFormattedTextField(this.formatter);
+    this.customBase.setEnabled(false);
+    this.customBase.setToolTipText("base");
+    this.customBase.setBounds(10, 71, 35, 20);
+    panel.add(this.customBase);
+    
+    this.customAltura = new JFormattedTextField(this.formatter);
+    this.customAltura.setEnabled(false);
+    this.customAltura.setToolTipText("altura");
+    this.customAltura.setBounds(50, 71, 42, 20);
+    panel.add(this.customAltura);
+    
+    this.customBase.setText("1920");
+    this.customAltura.setText("1080");
+    
+    JLabel label = new JLabel("16:9");
+    label.setBounds(10, 98, 46, 14);
+    panel.add(label);
+    
+    this.customBaseEdit = new JFormattedTextField(this.formatter);
+    this.customAlturaEdit = new JFormattedTextField(this.formatter);
+    
+    this.resolucoesList.addActionListener(configResolucao());
+  }
 }
